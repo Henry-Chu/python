a0f508d935f9625d372e665a24e7ed39d5d85b1b
Henry-Chu <80148066+Henry-Chu@users.noreply.github.com>
2021-03-20 17:13:04 +0800

    class3

---
+++ class/3/password.py
@@ -0,0 +1,7 @@
+name=input('pleas input your name')
+password=inpuif (name=='garbage')t('pleas input your password')
+color=input('pleas input your color')
+if (name=='garbage') & (password == 'ppprrr') & (color=='red') :
+    print('correct')
+else:
+    print('wrong')
