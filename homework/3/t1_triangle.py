cf3bd4b9d37372fba225a563a3e1751d5c0aa1d9
Henry-Chu <80148066+Henry-Chu@users.noreply.github.com>
2021-03-24 22:41:25 +0800

    HW3

--- homework/3/t1_triangle.py
+++ homework/3/t1_triangle.py
@@ -1,8 +1,7 @@
 """
 Topic:輸入三角形三邊，判斷是否能構成三角形，
 　　　是三角形則顯示面積和周長，不行則顯示，無法構成三角形:
-
-Circle Area formula: 
+Triangle Area formula:
 p = 1/2 (a+b+c)
 area = (p * (p - a) * (p - b) * (p - c)) ** 0.5
 
@@ -20,3 +19,16 @@ output:
 perimeter: 12.000000
 Area: 6.000000
 """
+a=input("a=?")
+b=input("b=?")
+c=input("c=?")
+a=int(a)
+b=int(b)
+c=int(c)
+p =1/2*(a+b+c)
+area = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+if (c<a+b)and(c>b-a):
+    print(a+b+c)
+    print(area)
+else:
+    print("can't make a triangle")
