153c87c974f2c787da35cce061553633f30cddfb
Henry-Chu <80148066+Henry-Chu@users.noreply.github.com>
2021-03-24 21:47:06 +0800

    HW3

--- homework/3/t0_fraction.py
+++ homework/3/t0_fraction.py
@@ -12,3 +12,12 @@ Input1:6
 Input2:9
 Output:False
 """
+print("350/450=?")
+a=input("Please input numerator.")
+b=input("Please input denominator.")
+a=int(a)
+b=int(b)
+if a/b==350/450:
+    print("True")
+else:
+    print("False")
