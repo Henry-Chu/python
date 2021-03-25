4b72a9b4ef5511dde45dfa2f8ae703a19f70d14c
Henry-Chu <80148066+Henry-Chu@users.noreply.github.com>
2021-03-25 21:33:07 +0800

    HW3

--- homework/3/t2_climate (1).py
+++ homework/3/t2_climate (1).py
@@ -1,8 +1,16 @@
 """
-Topic:輸入溫度，如果溫度>=40度C,顯示: 太熱, 
+Topic:輸入溫度，如果溫度>=40度C,顯示: 太熱,
 　　　如果溫度<= 10 顯示:太冷, 其他：舒適:
 
-Show:Please input temperature:"
-Input1:40
+Show:Please input temperature :"
+Input1:40temperature
 Output:It's too hot.
 """
+temperature=input('Please input temperature')
+temperature=int(temperature)
+if temperature>=40:
+    print('It is too hot.')
+elif temperature<=10:
+    print('It is too cold.')
+else:
+    print("It is Comfortable.")
